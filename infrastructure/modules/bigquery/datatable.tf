cde269ff2e63f312a9f92afa18be5a14887ae225
Karthikey Surineni <karthikey.surineni@auspost.com.au>
2020-04-05 16:43:15 +1000

    fix

--- infrastructure/modules/bigquery/datatable.tf
+++ infrastructure/modules/bigquery/datatable.tf
@@ -2,15 +2,12 @@ resource "google_bigquery_table" "datatable" {
   for_each = toset(var.table_list)
   dataset_id = google_bigquery_dataset.dataset.dataset_id
   table_id   = each.key
-
   time_partitioning {
     type = "DAY"
     field = lookup(var.partition_config,each.key,var.default_partition_field)
   }
-
   labels = {
     env = var.env
-
+  }
   schema = file("${path.cwd}/modules/bigquery/schema/${each.key}.json")
-}
 }
