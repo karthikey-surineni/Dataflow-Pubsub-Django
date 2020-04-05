e612ff1760fc4deaee7f7db2a0e4b6a8081af4b2
Karthikey Surineni <karthikey.surineni@auspost.com.au>
2020-04-05 11:56:07 +1000

    split backend check to separate step

---
+++ infrastructure/deploy_backend.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BACKEND_BUCKET="${PROJECT}-backend"
+PLAN="${PROJECT}-${ENV}.out"
+
+create_backend(){
+    $(gsutil mb -c standard -l australia-southeast1 gs://${BACKEND_BUCKET})
+}
+
+# Helper functions
+check_backend_exists(){
+for item in $(gsutil ls)
+do
+    if [ $item == "gs://$BACKEND_BUCKET/" ]; then
+        found=1
+    fi
+done
+
+}
+
+check_backend_exists
+[ -z $found ] && create_backend; exit 0 || exit 0
--- infrastructure/terraform-util.sh
+++ infrastructure/terraform-util.sh
@@ -7,34 +7,16 @@ PLAN="${PROJECT}-${ENV}.out"
 echo $BACKEND_BUCKET
 echo $PLAN
 
-# Helper functions
-check_backend_exists(){
-for item in $(gsutil ls)
-do
-    if [ $item == "gs://$BACKEND_BUCKET/" ]; then
-        found=1
-    fi
-done
-}
-create_backend(){
-    $(gsutil mb -c standard -l australia-southeast1 gs://${BACKEND_BUCKET})
-}
-configure_backend(){
-    terraform init \
-    -backend-config=bucket="${BACKEND_BUCKET}" \
-    -reconfigure -get=false;
-    terraform workspace new "${ENV}";
-    terraform workspace select "${ENV}"
-}
-
 # Terraform functions
 get(){
     terraform get -update=true
 }
 init(){
-    check_backend_exists
-    [ -z $found ] && create_backend; configure_backend || configure_backend
-}
+    terraform init \
+    -backend-config=bucket="${BACKEND_BUCKET}" \
+    -reconfigure -get=false;
+    terraform workspace new "${ENV}";
+    terraform workspace select "${ENV}"}
 
 plan(){
     terraform plan \
